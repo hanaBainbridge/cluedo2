bcfb2f70ec8b7e72d7b58d86a8e340cff4131f4e
hanaBainbridge <hanabainbridge@hotmail.co.nz>
2016-08-23 14:12:52 +1200

    small change somewhere

--- src/game_elements/Board.java
+++ src/game_elements/Board.java
@@ -1,4 +1,3 @@
-//hi connor
 package game_elements;
 
 import java.awt.Point;
@@ -122,11 +121,11 @@ public class Board {
 			weaponCards.remove(randomIndex); // Removes the solution weapon.
 		} 
 
-		randomIndex = (int) (Math.random() * 8) + 1;
+		randomIndex = (int) (Math.random() * 5) + 1;
 		RoomCard room = roomCards.get(randomIndex); // Selects a random room card.
 		if (startingSol) {
 			roomCards.remove(randomIndex);
-		}
+		}   
 		return new Solution(character, weapon, room); // Returns the solution object.
 	}
 	
